bfd056c32ff9b81462aa5e73714ecde3cfa41e68
Tomás Jiménez <t.jimenez03@ufromail.cl>
2019-11-11 03:27:10 -0300

    Update notificaciones.js

--- api/routes/notificaciones.js
+++ api/routes/notificaciones.js
@@ -20,10 +20,9 @@ router.get("/savetoken/:firebase_token", require('../middlewares/checksession'),
     const userid = req.userid;
 
     if (firebase_token == null && userid == null) return res.send({ response: false });
-    await Usuario.query().patch({ firebase_token }).where("id", userid);
-    return res.send({
-        response: true
-    });
+    let result = await Usuario.query().patch({ firebase_token }).where("id", userid);
+    if (result) return res.send({ response: true });
+    else res.send({ response: false });
 });
 
 router.get("/types", (req, res) => {
@@ -38,7 +37,8 @@ router.post("/send/:notification_type", async (req, res) => {
     const { patente } = req.body;
     if (patente == null) return res.send({ response: false });
     const patenteInfo = await Patente.query().where("patente", patente);
-    const user = await Usuario.query().where("id", patenteInfo[0].user_id);
+    if (patenteInfo.length == 0) return res.send({ response: false, error: "Patente no registrada" });
+    const user = await Usuario.query().where("id", patenteInfo[0].usuario_id);
     if (user[0].token == null || user[0].token == '') return res.send({ response: false, error: "El usuario no tiene una sesión activa" });
     const notification_key = user[0].firebase_token;
     if (notification_key == null && notification_key == '') return res.send({ response: false, error: "no tiene key" });

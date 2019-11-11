62e05224b8ea1d5e9cd6febbf1175b46933c9126
Tomás Jiménez <t.jimenez03@ufromail.cl>
2019-11-11 02:47:20 -0300

    Update 02_usuario.js

--- api/seeds/02_usuario.js
+++ api/seeds/02_usuario.js
@@ -39,7 +39,7 @@ exports.seed = function (knex, Promise) {
           user: "usuario",
           password: cryptPassword("password"),
           token: null,
-          firebase_token: 'fwucdRDGLAA:APA91bGbXMUH6-iFaCtS7tBsT3u_n3Rt0cNgXh0XlYOBMeApUasO0cGLR7JQGkBsatwlfNSiMp-x3dzvw6Vyum4eBGIEjSUw3KiyXnXI46bQy9frGiBH4c8xMPaE6S4AcfHtxHWMEJUR',
+          firebase_token: 'e1RT3FLNa6Q:APA91bGH7RA-I_O_psUf5Ggno2FVEv0Z2-kaRrPr6h6YABHcKhi1c0aUwRESdRdgLymk-boaSrygjlMs4eroEkp4IVvogCd7FKiCF5kFPcnTxtt9-stCZ6PU1MkGbyq4xcj7O_yy0v83',
           tipo_usuario_id: 3
         }
       ]);

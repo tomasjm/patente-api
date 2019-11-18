2101b9d0e68fdf228e255433b7fd819f68817499
Tomás Jiménez <tjimenez999@gmail.com>
2019-11-18 01:24:27 -0300

    adm fixes

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -5,13 +5,13 @@ const Usuario = require("../models/Usuario");
 const Patente = require("../models/Patente");
 
 router.get("/estadisticas", async (req, res) => {
-    let userCount = await Usuario.query().count("id");
-    let patenteCount = await Patente.query().count("id");
+    let userCount = await Usuario.query().count("id as a");
+    let patenteCount = await Patente.query().count("id as p");
     return res.send({
         response: true,
         data: {
-            user_count: userCount,
-            patente_count, patenteCount
+            user_count: userCount[0].a,
+            patente_count: patenteCount[0].p
         }
     });
 });

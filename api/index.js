0542626e7bfe546c4eaf696bdc14ea6bb711acd6
Tomás Jiménez <tjimenez999@gmail.com>
2019-11-18 00:56:56 -0300

    adm

--- api/index.js
+++ api/index.js
@@ -43,6 +43,7 @@ app.use(
 app.use("/patentes", require("./routes/patentes"));
 app.use("/notifications", require("./routes/notificaciones"));
 app.use("/instituciones", require("./routes/institucion"));
+app.use("/admin", require("./routes/admin"));
 // app.use('/eventos', require("./middlewares/userchecker"), require("./routes/eventos"));
 // app.use('/tipo', require("./routes/tipo"));
 // app.use('/landing', require("./routes/landing"));

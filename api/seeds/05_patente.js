1d2e17233336eae3655ca9103371ba7e69e70d43
Tomás Jiménez <tjimenez999@gmail.com>
2019-11-21 19:38:25 -0300

    regex change

--- api/seeds/05_patente.js
+++ api/seeds/05_patente.js
@@ -5,7 +5,7 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex("patente").insert([
-        { id: 1, patente: "ABCD-1234", desc: "descripcion", usuario_id: 3 }
+        { id: 1, patente: "AABB12", desc: "descripcion", usuario_id: 3 }
       ]);
     });
 };
